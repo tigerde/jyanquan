182ee5083639b0ef434ecdfe10ad68e786ba8e49
mooncake <tigerde126@126.com>
2013-11-14 17:07:21 +0800

    更改

---
+++ jyanquan/src/com/anquan/jy/action/BehaviorAction.java
@@ -0,0 +1,175 @@
+package com.anquan.jy.action;
+
+import net.sf.json.JSONObject;
+
+import com.anquan.jy.biz.DriverInfoBiz;
+import com.anquan.jy.entity.Behavior;
+import com.anquan.jy.entity.TrafficViolation;
+import com.anquan.jy.util.TimeFormatUtil;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BehaviorAction extends ActionSupport {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int pageNo;// 页码
+	private int pageSize;// 页值
+	private String sidx;// 排序字段
+	private String sord;// 排序方式 asc /desc
+	private boolean _search=false;// 是否搜索 true false
+	private String oper; // 增删改判断标记
+	private String id;
+	private JSONObject json;
+	
+	private String idcard;// '身份证',
+	private String name;// '驾驶员',
+	private String incident_date;// '时间',
+	private String incident_location;// '地点',
+	private String description;// '经过',
+	private String result;// '结果',
+	private int type;// '行为类型：1良好行为，2不良行为',
+	
+	
+	
+	public String getDriverTrafficViolation(){
+		json=DriverInfoBiz.getTrafficViolations(pageNo, pageSize, sidx, sord, id);
+		return SUCCESS;
+	}
+	/**
+	 * 编辑驾驶员交通事故信息
+	 */
+	public void editTrafficViolation(){
+		if (oper.equals("del")){
+			//删除
+			//DriverInfoDao.delTrafficViolation(id);
+			
+		}else if (oper.equals("edit")) {
+			//编辑
+			Behavior be=new Behavior();
+			TrafficViolation ta=new TrafficViolation();
+			be.setId(id);
+			be.setIdcard(idcard);
+			
+			be.setName(name);
+			be.setIncident_date(incident_date);
+			be.setIncident_location(incident_location);
+			be.setDescription(description);
+			be.setResult(result);
+			be.setType(type);
+			
+			be.setModifi_datetime(TimeFormatUtil.getcurrentTime());
+			be.setModifi_user_id("1");
+			// DriverInfoDao.updateTrafficViolation(ta);
+		}else if(oper.equals("add")){
+			Behavior be=new Behavior();
+			TrafficViolation ta=new TrafficViolation();
+			be.setIdcard(idcard);
+
+			be.setName(name);
+			be.setIncident_date(incident_date);
+			be.setIncident_location(incident_location);
+			be.setDescription(description);
+			be.setResult(result);
+			be.setType(type);
+			
+			be.setModifi_datetime(TimeFormatUtil.getcurrentTime());
+			be.setModifi_user_id("1");
+			be.setCreate_datetime(TimeFormatUtil.getcurrentTime());
+			ta.setCreate_user_id("1");
+			//DriverInfoDao.insertTrafficViolation(ta);
+		}
+	}
+	public int getPageNo() {
+		return pageNo;
+	}
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public String getSidx() {
+		return sidx;
+	}
+	public void setSidx(String sidx) {
+		this.sidx = sidx;
+	}
+	public String getSord() {
+		return sord;
+	}
+	public void setSord(String sord) {
+		this.sord = sord;
+	}
+	public boolean is_search() {
+		return _search;
+	}
+	public void set_search(boolean search) {
+		_search = search;
+	}
+	public String getOper() {
+		return oper;
+	}
+	public void setOper(String oper) {
+		this.oper = oper;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public JSONObject getJson() {
+		return json;
+	}
+	public void setJson(JSONObject json) {
+		this.json = json;
+	}
+	public String getIdcard() {
+		return idcard;
+	}
+	public void setIdcard(String idcard) {
+		this.idcard = idcard;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIncident_date() {
+		return incident_date;
+	}
+	public void setIncident_date(String incidentDate) {
+		incident_date = incidentDate;
+	}
+	public String getIncident_location() {
+		return incident_location;
+	}
+	public void setIncident_location(String incidentLocation) {
+		incident_location = incidentLocation;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+}
---
+++ jyanquan/src/com/anquan/jy/dao/TrafficViolationDao.java
@@ -0,0 +1,269 @@
+package com.anquan.jy.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.anquan.jy.entity.TrafficAccident;
+import com.anquan.jy.entity.TrafficViolation;
+import com.anquan.jy.util.DBConn;
+import com.anquan.jy.util.TimeFormatUtil;
+
+public class TrafficViolationDao {
+	private static final Logger log=Logger.getLogger(TrafficViolationDao.class);
+	
+	/**
+	 * 获取驾驶员交通违章信息列表
+	 * @param id 驾驶员db中id
+	 * @return
+	 */
+	public static List<TrafficViolation> getTrafficViolations(String id,String sql){
+		List<TrafficViolation> list=new ArrayList<TrafficViolation>();
+		Connection conn=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=DBConn.getConnection();
+			ps=conn.prepareStatement("select * from traffic_violation where  deleted=0 and idcard=? "+sql);
+			ps.setString(1, id);
+			rs=ps.executeQuery();
+			while(rs.next()){
+				
+				TrafficViolation ta=new TrafficViolation();
+				ta.setId(rs.getString("id"));
+				ta.setIdcard(rs.getString("idcard"));
+				ta.setCompany(rs.getString("company"));
+				ta.setBus_number(rs.getString("bus_number"));
+				ta.setName("");
+				ta.setViolation_date(rs.getString("violation_date"));
+				ta.setViolation_location(rs.getString("violation_location"));
+				ta.setViolation_reason(rs.getString("violation_reason"));
+				ta.setPoints(rs.getInt("points"));
+				ta.setFine(rs.getDouble("fine"));
+				ta.setAssessment_date(rs.getString("assessment_date"));
+				 
+				try {
+					ta.setCreate_datetime(TimeFormatUtil.getTime(rs.getString("create_datetime")));
+				}catch (ParseException e) {
+					log.error("getTrafficViolations时间格式化错误", e);
+				}catch (NullPointerException e){
+					log.error("getTrafficViolations时间格式化错误", e);
+				}
+				
+				try {
+					ta.setModifi_datetime(TimeFormatUtil.getTime(rs.getString("modifi_datetime")));
+				}catch (ParseException e) {
+					log.error("getTrafficViolations时间格式化错误", e);
+				}catch (NullPointerException e){
+					log.error("getTrafficViolations时间格式化错误", e);
+				}
+				
+				ta.setCreate_user_id(rs.getString("create_user_id"));
+				ta.setModifi_user_id(rs.getString("modifi_user_id"));
+				list.add(ta);
+			}
+			
+		} catch (SQLException e) {
+			log.error("getTrafficViolations获取驾驶员交通违章信息", e);
+		}finally{
+			DBConn.close(rs, ps, conn);
+		}
+		return list;
+	}
+	
+	
+	/**
+	 * 获取驾驶员交通违章信息
+	 * @param id
+	 * @return
+	 */
+	public static TrafficViolation getTrafficViolation(String id){
+		TrafficViolation ta=null;
+		Connection conn=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=DBConn.getConnection();
+			ps=conn.prepareStatement("select * from traffic_violation where deleted=0 and id=?");
+			ps.setString(1, id);
+			rs=ps.executeQuery();
+			if(rs.next()){
+				ta=new TrafficViolation();
+				ta.setId(rs.getString("id"));
+				ta.setIdcard(rs.getString("idcard"));
+				ta.setCompany(rs.getString("company"));
+				ta.setBus_number(rs.getString("bus_number"));
+				ta.setName("");
+				ta.setViolation_date(rs.getString("violation_date"));
+				ta.setViolation_location(rs.getString("violation_location"));
+				ta.setViolation_reason(rs.getString("violation_reason"));
+				ta.setPoints(rs.getInt("points"));
+				ta.setFine(rs.getDouble("fine"));
+				ta.setAssessment_date(rs.getString("assessment_date"));
+				
+				try {
+					ta.setCreate_datetime(TimeFormatUtil.getTime(rs.getString("create_datetime")));
+				}catch (ParseException e) {
+					log.error("getTrafficViolation时间格式化错误", e);
+				}catch (NullPointerException e){
+					log.error("getTrafficViolation时间格式化错误", e);
+				}
+				
+				try {
+					ta.setModifi_datetime(TimeFormatUtil.getTime(rs.getString("modifi_datetime")));
+				}catch (ParseException e) {
+					log.error("getTrafficViolation时间格式化错误", e);
+				}catch (NullPointerException e){
+					log.error("getTrafficViolation时间格式化错误", e);
+				}
+				
+				ta.setCreate_user_id(rs.getString("create_user_id"));
+				ta.setModifi_user_id(rs.getString("modifi_user_id"));
+				
+			}
+			
+		} catch (SQLException e) {
+			log.error("getTrafficViolation获取驾驶员交通违章信息", e);
+		}finally{
+			DBConn.close(rs, ps, conn);
+		}
+		return ta;
+	}
+	
+	/**
+	 * 删除某条交通违章信息
+	 * @param id
+	 * @return
+	 */
+	public static int delTrafficViolation(String id){
+		int i=0;
+		Connection conn=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=DBConn.getConnection();
+			ps=conn.prepareStatement("update traffic_violation set deleted=1 where id=? ");
+			ps.setString(1, id);
+			i=ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			log.error("delTrafficViolation删除驾驶员交通违章信息", e);
+		}finally{
+			DBConn.close(rs, ps, conn);
+		}
+		return i;
+	}
+	
+	/**
+	 * 更新驾驶员违章信息
+	 * @param ta
+	 * @return
+	 */
+	public static int updateTrafficViolation(TrafficViolation ta){
+		int i=0;
+		Connection conn=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=DBConn.getConnection();
+			ps=conn.prepareStatement("update traffic_violation set bus_number=?,company=?,violation_date=?,violation_location=?,violation_reason=?," +
+			"points=?,fine=?,assessment_date=?,modifi_datetime=?,modifi_user_id=?  where id=? ");
+			ps.setString(1, ta.getBus_number());
+			ps.setString(2, ta.getCompany());
+			ps.setString(3, ta.getViolation_date());
+			ps.setString(4, ta.getViolation_location());
+			ps.setString(5, ta.getViolation_reason());
+			ps.setInt(6, ta.getPoints());
+			ps.setDouble(7, ta.getFine());
+			ps.setString(8, ta.getAssessment_date());
+			ps.setString(9, ta.getModifi_datetime());
+			ps.setString(10, ta.getModifi_user_id());
+			ps.setString(11, ta.getId());
+			
+			i=ps.executeUpdate();
+			System.out.println("sdfghjkl;'"+i);
+		} catch (SQLException e) {
+			log.error("updateTrafficViolation更新驾驶员交通违章信息", e);
+		}finally{
+			DBConn.close(rs, ps, conn);
+		}
+		return i;
+	}
+	/**
+	 * 创建交通事故信息
+	 * @param ta
+	 * @return
+	 */
+	public static int insertTrafficViolation(TrafficViolation ta){
+		int i=0;
+		Connection conn=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=DBConn.getConnection();
+			ps=conn.prepareStatement("insert into  traffic_violation (bus_number,company,violation_date,violation_location,violation_reason," +
+			"points,fine,assessment_date,modifi_datetime,modifi_user_id,idcard,create_datetime,create_user_id,deleted) values(?,?,?,?,?,?,?,?,?,?,?,?,?,0) ");
+			ps.setString(1, ta.getBus_number());
+			ps.setString(2, ta.getCompany());
+			ps.setString(3, ta.getViolation_date());
+			ps.setString(4, ta.getViolation_location());
+			ps.setString(5, ta.getViolation_reason());
+			ps.setInt(6, ta.getPoints());
+			ps.setDouble(7, ta.getFine());
+			ps.setString(8, ta.getAssessment_date());
+			 
+			ps.setString(9, ta.getModifi_datetime());
+			ps.setString(10, ta.getModifi_user_id());
+			ps.setString(11, ta.getIdcard());
+			ps.setString(12, ta.getCreate_datetime());
+			ps.setString(13, ta.getCreate_user_id());
+			
+			i=ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			log.error("更新驾驶员交通事故信息", e);
+		}finally{
+			DBConn.close(rs, ps, conn);
+		}
+		return i;
+	}
+	/**
+	 * 获取某驾驶员交通违章数据总行数
+	 * @param id
+	 * @return
+	 */
+	public static int getTrafficViolationCount(String id){
+		int i=0;
+		Connection conn=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		
+		conn=DBConn.getConnection();
+		try {
+			ps=conn.prepareStatement("select count(*) count from traffic_violation where idcard=? and deleted=0");
+			ps.setString(1, id);
+			rs=ps.executeQuery();
+			if(rs.next()){
+				i=rs.getInt("count");
+			}
+		} catch (SQLException e) {
+			log.error("getTrafficViolationCount获取驾驶员违章信息条数", e);
+		}finally{
+			DBConn.close(rs, ps, conn);
+		}
+		
+		return i;
+	}
+	
+}
